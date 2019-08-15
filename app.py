04816850a7615c5b07106db47176b56540ca56ca
sopa3108 <52861313+sopa3108@users.noreply.github.com>
2019-08-15 13:02:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-    if (userText == 'ไปเถอะ') :
-       sendText(user,'ไปไหนอ่ะ')
+    if (userText == 'สวัสดี') :
+       sendText(user,'เออ ดีด้วย')
     elif (userText == 'ไปนะ') :
-       sendText(user,'ชวนจริ้งงงง')
+       sendText(user,'ไปตายไหนก็ไป')
     else :
        sendText(user,'ว่าไงนะ')
 
